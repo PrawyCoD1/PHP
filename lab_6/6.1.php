7ee5692da18a52ac624aa7fd8b1642f9fd099f9c
Jakub Kleemann <s26995@pjwstk.edu.pl>
2023-04-07 16:41:01 +0200

    Undo revert of the code

--- lab_6/6.1.php
+++ lab_6/6.1.php
@@ -1,38 +1,37 @@
-?>echo reverseLine();
-}
-    }
-        }
-            }
-            fwrite($open, $file[$j]);
-            else{
-            }
-                fwrite($open, $file[$j] . "\n");
-            if($j == 3){ //That's for the case when there's no new line in the last line. If there is, output is going to be messed up
-        for($j = sizeof($file) - 1; $j >=0; $j--){
-        $open = fopen($pick_file, "w");
-        }
-            echo "{$file[$i]}</br>";
-        for($i = 0; $i < sizeof($file); $i++){
-        echo "Preview of text in the file:</br>";
-        $file = file($pick_file);
-        $pick_file = $_POST["pick_file"];
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-{
-function reverseLine()
-
-<?php
-</html>
-
-</body>
-    </form>
-        <input type="submit">
-        <input type="file" name="pick_file" value ="" required/></br></br>
-    <form method="post">
-<body>
-
-</head>
-    <title>SRAKA</title>
+<html>
 
 <head>
 
-<html>
+    <title>Reverse function</title>
+</head>
+
+<body>
+    <form method="post">
+        <input type="file" name="pick_file" required/></br></br>
+        <input type="submit">
+    </form>
+</body>
+</html>
+<?php
+function reverseLine()
+{
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["pick_file"])) {
+        $pick_file = $_POST["pick_file"];
+        $file = file($pick_file);
+        echo "Preview of text in the file:</br>";
+        for($i = 0; $i < sizeof($file); $i++){
+            echo $file[$i]."</br>";
+        }
+        $open = fopen($pick_file, "w");
+        for($j = sizeof($file) - 1; $j >=0; $j--){
+            if($j == 3){ //That's for the case when there's no new line in the last line. If there is, output is going to be messed up
+                fwrite($open, $file[$j] . "\n");
+            }
+            else{
+            fwrite($open, $file[$j]);
+            }
+        }
+    }
+}
+echo reverseLine();
+?>
